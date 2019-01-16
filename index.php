9a502217560d89aa42a8aa08238c3df88f421f27
wahyuarthanugraha <46750780+wahyuarthanugraha@users.noreply.github.com>
2019-01-16 22:20:44 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<!--
+Tinker Template
+http://www.templatemo.com/tm-506-tinker
+-->
+        <title>BPJS Kesehatan</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="img/logo.png" rel="icon">
+        
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/fontAwesome.css">
+        <link rel="stylesheet" href="css/hero-slider.css">
+        <link rel="stylesheet" href="css/owl-carousel.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <link rel="stylesheet" href="css/lightbox.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+
+<body>
+    <div class="header">
+        <div class="container">
+            <nav class="navbar navbar-inverse" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+					</p>
+                    <a href="#" class="navbar-brand scroll-top"><img class="navbar-brand scroll-top"src="img/logo.png"><em>BPJS</em>KESEHATAN</img></a>
+                </div>
+                <!--/.navbar-header-->
+                <div id="main-nav" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#" class="scroll-top">Home</a></li>
+                        <li><a href="#" class="scroll-link" data-id="about">Layanan</a></li>
+                        <li><a href="#" class="scroll-link" data-id="contact-us">Kontak</a></li>
+                    </ul>
+                </div>
+                <!--/.navbar-collapse-->
+            </nav>
+            <!--/.navbar-->
+        </div>
+        <!--/.container-->
+    </div>
+    <!--/.header-->
+
+
+    <div class="parallax-content baner-content" id="home">
+        <div class="container">
+            <div class="text-content">
+                <h2><em>KERJA BERSAMA</em> <span>SEHAT</span> BERSAMA</h2>
+                <p>Menularkan kebaikan dari diri kita untuk jutaan orang lain. Kesehatan adalah investasi yang berharga. Masyarakat yang sehat membentuk bangsa yang kuat.</p>
+                <div class="primary-blue-button">
+                    <a href="#" class="scroll-link" data-id="about">Ayo Daftar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <section id="about" class="page-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="service-item">
+                        <div class="icon">
+                            <img src="img/service_icon_01.png" alt="">
+                        </div>
+                        <h4>Pendaftaran Peserta</h4>
+                        <div class="line-dec"></div>
+                        <p>BPJS Kesehatan kapan pun dapat mengubah syarat dan ketentuan penggunaan Layanan Pendaftaran BPJS Kesehatan yang akan berlaku kepada seluruh pengguna Website Layanan Pendaftaran BPJS Kesehatan.</p>
+                        <div class="primary-blue-button">
+                            <a href="peserta/index.php" data-id="portfolio" target="blank">Lanjutkan</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="service-item">
+                        <div class="icon">
+                            <img src="img/service_icon_04.png" alt="">
+                        </div>
+                        <h4>BPJS Ketenagakerjaan</h4>
+                        <div class="line-dec"></div>
+                        <p>Badan Penyelenggara Jaminan Sosial (BPJS) Ketenagakerjaan merupakan badan hukum publik yang bertugas melindungi seluruh pekerja melalui 4 program jaminan sosial ketenagakerjaan</p>
+                        <div class="primary-blue-button">
+                            <a href="bpjsketenagakerjaan/index.html" data-id="portfolio" target="blank">Lanjutkan</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="service-item">
+                        <div class="icon">
+                            <img src="img/service_icon_02.png" alt="">
+                        </div>
+                        <h4>Asuransi Kesehatan</h4>
+                        <div class="line-dec"></div>
+                        <p>Asuransi kesehatan penting untuk dimiliki pada saat ini. Salah satunya karena produk yang ditawarkan dapat diterima di banyak rumah sakit dan bisa memberikan perlindungan berbagai risiko kesehatan.</p>
+                        <div class="primary-blue-button">
+                            <a href="asuransi/index.php" class="" data-id="portfolio" target="blank">Lanjutkan</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div id="contact-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-heading">
+                        <h4>Kontak</h4>
+                        <div class="line-dec"></div>
+                        <p>BPJS Kesehatan Kantor Pusat: JL Letjen Suprapto Cempaka Putih PO BOX 1391 JKT 10510 021-4212938 (Hunting)</p>
+                        <div class="pop-button">
+                            <h4>Kirim Pesan</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="pop">
+                        <span>✖</span>
+                        <form id="contact" action="#" method="post">
+                            <div class="row">
+                                <div class="col-md-12">
+                                  <fieldset>
+                                    <input name="name" type="text" class="form-control" id="name" placeholder="Your name..." required="">
+                                  </fieldset>
+                                </div>
+                                <div class="col-md-12">
+                                  <fieldset>
+                                    <input name="email" type="email" class="form-control" id="email" placeholder="Your email..." required="">
+                                  </fieldset>
+                                </div>
+                                <div class="col-md-12">
+                                  <fieldset>
+                                    <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your message..." required=""></textarea>
+                                  </fieldset>
+                                </div>
+                                <div class="col-md-12">
+                                  <fieldset>
+                                    <button type="submit" id="form-submit" class="btn">Kirim Pesan</button>
+                                  </fieldset>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-12">
+                    <div class="logo">
+                        <a class="logo-ft" href="#" class="scroll-top"><em>BPJS</em>Kesehatan</a>
+                        <p>Copyright &copy; 2019 Informatika Kelas A
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-12">
+                    <div class="location">
+                        <h4>Lokasi</h4>
+                        <ul>
+                            <li>Kantor Pusat, <br>Jakarta 10510</li>
+                            <li>Kantor Cabang, <br>Parepare 91122</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-12">
+                    <div class="contact-info">
+                        <h4>More Info</h4>
+                        <ul>
+                            <li><em>Phone</em>: 021-4212938</li>
+                            <li><em>Webmail</em>: mail.bpjs-kesehatan.go.id</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-12">
+                    <div class="connect-us">
+                        <h4>Sosial Media</h4>
+                        <ul>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+    <script src="js/vendor/bootstrap.min.js"></script>
+
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+        // navigation click actions 
+        $('.scroll-link').on('click', function(event){
+            event.preventDefault();
+            var sectionID = $(this).attr("data-id");
+            scrollToID('#' + sectionID, 750);
+        });
+        // scroll to top action
+        $('.scroll-top').on('click', function(event) {
+            event.preventDefault();
+            $('html, body').animate({scrollTop:0}, 'slow');         
+        });
+        // mobile nav toggle
+        $('#nav-toggle').on('click', function (event) {
+            event.preventDefault();
+            $('#main-nav').toggleClass("open");
+        });
+    });
+    // scroll function
+    function scrollToID(id, speed){
+        var offSet = 50;
+        var targetOffset = $(id).offset().top - offSet;
+        var mainNav = $('#main-nav');
+        $('html,body').animate({scrollTop:targetOffset}, speed);
+        if (mainNav.hasClass("open")) {
+            mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+            mainNav.removeClass("open");
+        }
+    }
+    if (typeof console === "undefined") {
+        console = {
+            log: function() { }
+        };
+    }
+    </script>
+</body>
+</html>
